191e2bfe709071a3a23bdfb690d743b29850fca2
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-14 11:36:57 +0400

    commiting for travis build

---
+++ scripts/travis-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker run -e TRAVIS_BRANCH -e ENCRYPTION_LABEL -i -t -v `pwd`:/nyuad-conda-configs jerowe/nyuad-anaconda /bin/bash ./deploy.sh
---
+++ scripts/travis-setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+docker pull jerowe/nyuad-anaconda
