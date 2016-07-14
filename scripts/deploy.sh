4249e5c618fd9cbd6eb17e08c5abde4227033486
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-14 16:56:17 +0400

    trying again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,36 +8,36 @@ set -e # Exit with nonzero exit code if anything fails
 	#exit 0
 #fi
 
-yum install -y git
-export PATH=/anaconda/bin:$PATH
+#yum install -y git
+#export PATH=/anaconda/bin:$PATH
 
-cd /nyuad-conda-configs
+#cd /nyuad-conda-configs
 
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-git checkout $TRAVIS_BRANCH
+#git checkout $TRAVIS_BRANCH
 
-git config user.name "Jillian Rowe"
-git config user.email "jillian.e.rowe@gmail.com"
+#git config user.name "Jillian Rowe"
+#git config user.email "jillian.e.rowe@gmail.com"
 
-### Begin Run Tests
+#### Begin Run Tests
 
-echo "This is a TEST!" >> test.md
+#echo "This is a TEST!" >> test.md
 
-### End Run Tests
+#### End Run Tests
 
 git add -A
 git commit -m "Deploy MD5 sum checks: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-export ENCRYPTION_LABEL="33e9f3ccc52a"
-export COMMIT_AUTHOR_EMAIL="jillian.e.rowe@gmail.com"
-export ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-export ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-export ENCRYPTED_KEY="${!ENCRYPTED_KEY_VAR}"
-export ENCRYPTED_IV="${!ENCRYPTED_IV_VAR}"
+## Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+#export ENCRYPTION_LABEL="33e9f3ccc52a"
+#export COMMIT_AUTHOR_EMAIL="jillian.e.rowe@gmail.com"
+#export ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+#export ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+#export ENCRYPTED_KEY="${!ENCRYPTED_KEY_VAR}"
+#export ENCRYPTED_IV="${!ENCRYPTED_IV_VAR}"
 
 #openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in nyuad-hpc-module-configs-deploy.enc -out nyuad-hpc-module-configs-deploy -d
 #openssl aes-256-cbc -K $encrypted_33e9f3ccc52a_key -iv $encrypted_33e9f3ccc52a_iv -in nyuad-hpc-module-configs-deploy.enc -out nyuad-hpc-module-configs-deploy -d
--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
-    docker run -e TRAVIS_BRANCH -e ENCRYPTION_LABEL -i -t -v `pwd`:/nyuad-conda-configs jerowe/nyuad-anaconda /nyuad-conda-configs/scripts/deploy.sh
+    docker run -e TRAVIS_BRANCH -e ENCRYPTION_LABEL -i -t -v `pwd`:/nyuad-conda-configs jerowe/nyuad-anaconda /nyuad-conda-configs/scripts/run-tests.sh
+    ./scripts/deploy.sh
 else
-
     exit 0
 fi
