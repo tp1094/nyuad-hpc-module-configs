b0fd367be30ae21d366bc590c549bfa7c5ebd194
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-14 15:12:54 +0400

    commiting

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -2,4 +2,10 @@
 
 set -euo pipefail
 
-docker run -e TRAVIS_BRANCH -e ENCRYPTION_LABEL -i -t -v `pwd`:/nyuad-conda-configs jerowe/nyuad-anaconda ./scripts/deploy.sh
+if [[ $TRAVIS_OS_NAME = "linux" ]]
+then
+    docker run -e TRAVIS_BRANCH -e ENCRYPTION_LABEL -i -t -v `pwd`:/nyuad-conda-configs jerowe/nyuad-anaconda pwd
+else
+
+    exit 0
+fi
