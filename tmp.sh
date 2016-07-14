ec3a760358f3cb7a9695361298a6b1d5aa7135d6
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-14 16:56:34 +0400

    trying again

--- tmp.sh
+++
@@ -1,6 +0,0 @@
-export ENCRYPTION_LABEL="33e9f3ccc52a"
-export ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-export ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-export ENCRYPTED_KEY="${!ENCRYPTED_KEY_VAR}"
-export ENCRYPTED_IV="${!ENCRYPTED_IV_VAR}"
-#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in nyuad-hpc-module-configs-deploy.enc -out nyuad-hpc-module-configs-deploy -d
