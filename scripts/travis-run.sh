ce3aeec245feb4d209c98e0fa21ced881e7bb748
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-14 14:46:52 +0400

    commiting

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-docker run -e TRAVIS_BRANCH -e ENCRYPTION_LABEL -i -t -v `pwd`:/nyuad-conda-configs jerowe/nyuad-anaconda /bin/bash /nyuad-conda-configs/scripts/deploy.sh
+docker run -e TRAVIS_BRANCH -e ENCRYPTION_LABEL -i -t -v `pwd`:/nyuad-conda-configs jerowe/nyuad-anaconda /bin/bash ./scripts/deploy.sh
