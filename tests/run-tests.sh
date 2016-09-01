9bb5bcf2f0c3646a0e99e72b3fdb182a1ad3d03e
Travis CI <jillian.e.rowe@gmail.com>
2016-09-01 09:09:41 +0400

    just rebuild variant detection

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -12,9 +12,11 @@ if [[ $TRAVIS_BRANCH = "master" && "$TRAVIS_PULL_REQUEST" = false ]]
 then
     #Upload packages
     #TODO One script for testing and one for uploading
+    
     echo "Uploading packages to anaconda!"
     #python3 tests/test_environments.py --master --force_rebuild
     python3 tests/test_environments.py --master --environment recipes/variant_detection/1.0/environment-1.0.yml --force_rebuild
+
     #We will add this back soon
     #python3 scripts/build_docs.py --master
     #scripts/build_docs.sh
