330c58259d56f4113e45c70847c782a365be3a84
jerowe <jillian.e.rowe@gmail.com>
2017-01-31 10:08:22 +0400

    and forgot it again

--- tests/travis-run.sh
+++ tests/travis-run.sh
@@ -8,7 +8,7 @@ then
     docker run -e TRAVIS_PULL_REQUEST -e TRAVIS_BRANCH \
         -e GITHUB_TOKEN -e GITHUB_USER -e GITHUB_REPO \
         -e ANACONDA_TOKEN -e ANACONDA_PASSWORD -e ANACONDA_USER \
-        -i -t -v `pwd`:/nyuad-conda-configs nyuad_cgsb/anaconda-centos /nyuad-conda-configs/tests/run-tests.sh
+        -i -t -v `pwd`:/nyuad-conda-configs quay.io/nyuad_cgsb/anaconda-centos /nyuad-conda-configs/tests/run-tests.sh
 
 else
     exit 0
