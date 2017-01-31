f112a2a3b90073a9efc0b66de7ead64eef20ea5a
jerowe <jillian.e.rowe@gmail.com>
2017-01-31 09:52:37 +0400

    making some changes to the docker container

--- tests/travis-run.sh
+++ tests/travis-run.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
-    #Use docker container to run tests
 
-    docker run -e TRAVIS_PULL_REQUEST -e TRAVIS_BRANCH -e GITHUB_TOKEN -e GITHUB_USER -e GITHUB_REPO -e ANACONDA_TOKEN -e ANACONDA_PASSWORD -e ANACONDA_USER \
-        -i -t -v `pwd`:/nyuad-conda-configs jerowe/nyuad-anaconda /nyuad-conda-configs/tests/run-tests.sh
+    docker run -e TRAVIS_PULL_REQUEST -e TRAVIS_BRANCH \
+        -e GITHUB_TOKEN -e GITHUB_USER -e GITHUB_REPO \
+        -e ANACONDA_TOKEN -e ANACONDA_PASSWORD -e ANACONDA_USER \
+        -i -t -v `pwd`:/nyuad-conda-configs nyuad_cgsb/anaconda-centos /nyuad-conda-configs/tests/run-tests.sh
 
 else
     exit 0
